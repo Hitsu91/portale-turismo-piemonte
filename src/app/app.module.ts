f494f47109ffa09dec36272aecc8632f8563b558
Hitsu91 <lucafang@hotmail.it>
2020-02-02 14:46:00 +0100

    Minor fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,29 @@
 import { AgmCoreModule } from '@agm/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AgmDirectionModule } from 'agm-direction';
-import { AppRoutingModule } from './app-routing.module';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-
+import { AgmDirectionModule } from 'agm-direction';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContattaciComponent } from './contattaci/contattaci.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
-
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { ScopriComponent } from './scopri/scopri.component';
 import { CarouselComponent } from './home/carousel/carousel.component';
+import { ConsigliComponent } from './home/consigli/consigli.component';
 import { EventiComponent } from './home/eventi/eventi.component';
+import { HomeComponent } from './home/home.component';
 import { LuoghiSettimanaComponent } from './home/luoghi-settimana/luoghi-settimana.component';
 import { PiemonteDaScoprireComponent } from './home/piemonte-da-scoprire/piemonte-da-scoprire.component';
-import { ConsigliComponent } from './home/consigli/consigli.component';
-import { TruncateNewsPipe } from './pipes/truncate-news.pipe';
-import { MappaComponent } from './mappa/mappa.component';
 import { LuoghiComponent } from './luoghi/luoghi.component';
-import { MappaLeafletComponent } from './mappa-leaflet/mappa-leaflet.component';
-import { ContattaciComponent } from './contattaci/contattaci.component';
-import { LuogoComponent } from './luoghi/luogo/luogo.component';
 import { LuoghiService } from './luoghi/luoghi.service';
+import { LuogoComponent } from './luoghi/luogo/luogo.component';
+import { MappaLeafletComponent } from './mappa-leaflet/mappa-leaflet.component';
+import { MappaComponent } from './mappa/mappa.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { TruncateNewsPipe } from './pipes/truncate-news.pipe';
+import { ScopriComponent } from './scopri/scopri.component';
 
 @NgModule({
   declarations: [
